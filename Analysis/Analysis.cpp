e04d43a95d089c626037e18bccb7b100b8e36514
Caterina Aruta <caterinaaruta@MacBook-Pro-di-Caterina-44.local>
2019-07-04 19:39:44 +0200

    last version of analysis code

--- Analysis/Analysis.cpp
+++ Analysis/Analysis.cpp
@@ -6,24 +6,6 @@
 #include <iostream>
 #include <string.h>
 
-// ###### INSTRUCTIONS (input options)
-// (after having compiled the code [.L Analysis.cpp])
-//
-// * type = "MC" -> for the 'standard analysis' (triplet mass quite large)
-// * type = "MC_sgn" -> for the analysis for MVA  (triplet mass narrower)
-// *** Ds->Tau->3Mu : Analysis("MC", "Ds")
-// *** B0->Tau->3Mu : Analysis("MC", "B0")
-// *** Bp->Tau->3Mu : Analysis("MC", "Bp")
-// *** Ds->Phi->Pi  : Analysis("MC", "Ds_Phi")
-// *** Minimum bias : Analysis("MC", "MiniBias")
-//
-// * type = "data" -> for the 'standard analysis' (triplet mass quite large)
-// * type = "data_bkg" -> for the analysis for MVA (triplet mass complementary w.r.t. tau mass)
-// *** 2017 B : Analysis("data", "2017B")
-// *** 2017 C : Analysis("data", "2017C")
-// *** 2017 D : Analysis("data", "2017D")
-// *** 2017 F : Analysis("data", "2017F")
-
 using namespace std;
 
 int main(int narg, char** arg){
