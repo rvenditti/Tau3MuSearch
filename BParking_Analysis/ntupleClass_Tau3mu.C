9fed91851207aad1bdc4e9d0793878d88f9e102b
Caterina Aruta <caterinaaruta@MacBook-Pro-di-Caterina-44.local>
2020-05-08 08:01:38 +0200

    Bug fixed

--- BParking_Analysis/ntupleClass_Tau3mu.C
+++ BParking_Analysis/ntupleClass_Tau3mu.C
@@ -20,6 +20,13 @@ void ntupleClass_Tau3mu::LoopTau3mu(TString type, TString datasetName){
     // Variables definition & init
     int mu_Ind[NMU] = {0}, mu[NMU] = {0}, cutevt_onlyParking[NCUTS] = {0};
     bool isMC = false, miniAOD = false;
+    double w[6] = {0};
+    w[0] = 0.167;
+    w[1] = 0.186;
+    w[2] = 0.266;
+    w[3] = 0.521;
+    w[4] = 0.807;
+    w[5] = 1.;
     // Variables for the final tree
     double run_n = 0, lumi_n = 0, evt_n = 0, nHLT = 0, deltaR_max = 0, deltaZ_max = 0, Pmu3 = 0, cLP = 0, tKink = 0, segmComp = 0, tripletMass = 0, tripletMassReso = 0, fv_nC = 0, fv_dphi3D = 0, fv_d3D = 0, fv_d3Dsig = 0, d0 = 0, d0sig = 0, mindca_iso = 0, trkRel = 0, Pmu1 = 0, Ptmu1 = 0, etamu1 = 0, Pmu2 = 0, Ptmu2 = 0, etamu2 = 0, Ptmu3 = 0, etamu3 = 0, P_trip = 0, Pt_trip = 0, eta_trip = 0, nStationsMu1 = 0, nStationsMu2 = 0, nStationsMu3 = 0, Iso03Mu1 = 0, Iso03Mu2 = 0, Iso03Mu3 = 0, Iso05Mu1 = 0, Iso05Mu2 = 0, Iso05Mu3 = 0, nMatchesMu1 = 0, nMatchesMu2 = 0, nMatchesMu3 = 0, timeAtIpInOutMu_sig1 = 0, timeAtIpInOutMu_sig2 = 0, timeAtIpInOutMu_sig3 = 0, cQ_uS = 0, cQ_tK, cQ_gK = 0, cQ_tRChi2 = 0, cQ_sRChi2 = 0, cQ_Chi2LP = 0, cQ_Chi2LM = 0, cQ_lD = 0, cQ_gDEP = 0, cQ_tM = 0, cQ_gTP = 0, calEn_emMu1 = 0, calEn_emMu2 = 0, calEn_emMu3 = 0, calEn_hadMu1 = 0, calEn_hadMu2 = 0, calEn_hadMu3 = 0, caloComp = 0, fliDistPVSV_Chi2 = 0, isGlb1 = 0, isTracker1 = 0, isLoose1 = 0,  isSoft1 = 0, isPF1 = 0, isRPC1 = 0, isSA1 = 0, isCalo1 = 0, isGlb2 = 0, isTracker2 = 0, isLoose2 = 0,  isSoft2 = 0, isPF2 = 0, isRPC2 = 0, isSA2 = 0, isCalo2 = 0, isGlb3 = 0, isTracker3 = 0, isLoose3 = 0, isSoft3 = 0, isPF3 = 0, isRPC3 = 0, isSA3 = 0, isCalo3 = 0, vx1 = 0, vx2 = 0, vx3 = 0, vy1 = 0, vy2 = 0, vy3 = 0, vz1 = 0, vz2 = 0, vz3 = 0, Refvx1 = 0, Refvx2 = 0, Refvx3 = 0, Refvy1 = 0, Refvy2 = 0, Refvy3 = 0, Refvz1 = 0, Refvz2 = 0, Refvz3 = 0, SVx = 0, SVy = 0, SVz = 0, had03 = 0, had05 = 0, nJets03 = 0, nJets05 = 0, nTracks03 = 0, nTracks05 = 0, sumPt03 = 0, sumPt05 = 0, hadVeto03 = 0, hadVeto05 = 0, emVeto03 = 0, emVeto05 = 0, trVeto03 = 0, trVeto05 = 0, EnMu1 = 0, EnMu2 = 0, EnMu3 = 0, ChargeMu1 = 0, ChargeMu2 = 0, ChargeMu3 = 0, isQValid1 = 0, isTValid1 = 0, isIsoValid1 = 0, GLnormChi2_mu1 = 0, GL_nValidMuHits1 = 0, trkLayersWMeas1 = 0, nValidPixelHits1 = 0, outerTrk_P_1 = 0, outerTrk_Eta_1 = 0, outerTrk_normChi2_1 = 0, outerTrk_muStValidHits_1 = 0, innerTrk_P_1 = 0, innerTrk_Eta_1 = 0, innerTrk_normChi2_1 = 0, QInnerOuter_1 = 0, cQ_uS_1 = 0, cQ_tK_1 = 0, cQ_gK_1 = 0, cQ_tRChi2_1 = 0, cQ_sRChi2_1 = 0, cQ_Chi2LP_1 = 0, cQ_Chi2LM_1 = 0, cQ_lD_1 = 0, cQ_gDEP_1 = 0, cQ_tM_1 = 0, cQ_gTP_1 = 0, segmComp_1 = 0, caloComp_1 = 0, isQValid2 = 0, isTValid2 = 0, isIsoValid2 = 0, GLnormChi2_mu2 = 0, GL_nValidMuHits2 = 0, trkLayersWMeas2 = 0, nValidPixelHits2 = 0, outerTrk_P_2 = 0, outerTrk_Eta_2 = 0, outerTrk_normChi2_2 = 0, outerTrk_muStValidHits_2 = 0, innerTrk_P_2 = 0, innerTrk_Eta_2 = 0, innerTrk_normChi2_2 = 0, QInnerOuter_2 = 0, cQ_uS_2 = 0, cQ_tK_2 = 0, cQ_gK_2 = 0, cQ_tRChi2_2 = 0, cQ_sRChi2_2 = 0, cQ_Chi2LP_2 = 0, cQ_Chi2LM_2 = 0, cQ_lD_2 = 0, cQ_gDEP_2 = 0, cQ_tM_2 = 0, cQ_gTP_2 = 0, segmComp_2 = 0, caloComp_2 = 0, isQValid3 = 0, isTValid3 = 0, isIsoValid3 = 0, GLnormChi2_mu3 = 0, GL_nValidMuHits3 = 0, trkLayersWMeas3 = 0, nValidPixelHits3 = 0, outerTrk_P_3 = 0, outerTrk_Eta_3 = 0, outerTrk_normChi2_3 = 0, outerTrk_muStValidHits_3 = 0, innerTrk_P_3 = 0, innerTrk_Eta_3 = 0, innerTrk_normChi2_3 = 0, QInnerOuter_3 = 0, cQ_uS_3 = 0, cQ_tK_3 = 0, cQ_gK_3 = 0, cQ_tRChi2_3 = 0, cQ_sRChi2_3 = 0, cQ_Chi2LP_3 = 0, cQ_Chi2LM_3 = 0, cQ_lD_3 = 0, cQ_gDEP_3 = 0, cQ_tM_3 = 0, cQ_gTP_3 = 0, segmComp_3 = 0, caloComp_3 = 0;
     
@@ -58,32 +65,35 @@ void ntupleClass_Tau3mu::LoopTau3mu(TString type, TString datasetName){
         if(NGoodTriplets->at(0) < 1) continue;
         cutevt_onlyParking[0]++;
         // CUT 1 : Check HLT decision
-        int hlt_old = 0, hlt_parking = 0, hlt_fired = 0, hlt_Mu_fired[6] = {0}, goodTriplet = 0;
+        int hlt_old = 0, hlt_parking = 0, hlt[6] = {0}, hlt_Mu_fired[6] = {0}, goodTriplet = 0;
         bool good_muID = false, good_Chi2 = false; vector<int> ind_tripl;
         double rand = gRandom->Uniform();
        
         for(int h=0; h<Trigger_hltname->size(); h++) {
             TString hltName = Trigger_hltname->at(h);
-
             // std HLT
             if( hltName.Contains("HLT_DoubleMu3_TkMu_DsTau3Mu_v") && Trigger_hltdecision->at(h) == 1) { hlt_old = 1; }
             // Parking HLT
             if( (hltName.Contains("HLT_Mu7_IP4") || hltName.Contains("HLT_Mu8_IP") || hltName.Contains("HLT_Mu9_IP") || hltName.Contains("HLT_Mu12_IP")) && Trigger_hltdecision->at(h) == 1){
+                hlt_parking = 1;
+                if(hltName.Contains("HLT_Mu7_IP4")) hlt[0] = 1;
+                if(hltName.Contains("HLT_Mu8_IP3")) hlt[1] = 1;
+                if(hltName.Contains("HLT_Mu8_IP5")) hlt[2] = 1;
+                if(hltName.Contains("HLT_Mu9_IP5")) hlt[3] = 1;
+                if(hltName.Contains("HLT_Mu9_IP6")) hlt[4] = 1;
+                if(hltName.Contains("HLT_Mu12_IP6")) hlt[5] = 1;
+            }
+        } // end loop on HLT
+        
+        // Selecting HLT exclusively and reweighting MC
+        if(hlt_parking == 1){
+            for(int h=0; h<6; h++){
                 if(isMC == true){
-                    if(rand <= 0.167 && hltName.Contains("HLT_Mu7_IP4")) { hlt_parking = 1; hlt_Mu_fired[0] = 1; hHLT_deepStudy[1]->Fill(0); break; }
-                    if(rand > 0.167 && rand <= 0.186 && hltName.Contains("HLT_Mu8_IP3")) { hlt_parking = 1; hlt_Mu_fired[1] = 1; hHLT_deepStudy[1]->Fill(1); break; }
-                    if(rand > 0.186 && rand <= 0.266 && hltName.Contains("HLT_Mu8_IP5")) { hlt_parking = 1; hlt_Mu_fired[2] = 1; hHLT_deepStudy[1]->Fill(2); break; }
-                    if(rand > 0.266 && rand <= 0.521 && hltName.Contains("HLT_Mu9_IP5")) { hlt_parking = 1; hlt_Mu_fired[3] = 1; hHLT_deepStudy[1]->Fill(3); break; }
-                    if(rand > 0.521 && rand <= 0.807 && hltName.Contains("HLT_Mu9_IP6")) { hlt_parking = 1; hlt_Mu_fired[4] = 1; hHLT_deepStudy[1]->Fill(4); break; }
-                    if(rand > 0.807 && rand <= 1 && hltName.Contains("HLT_Mu12_IP6")) { hlt_parking = 1; hlt_Mu_fired[5] = 1; hHLT_deepStudy[1]->Fill(5); break; }
+                    if(h==0 && rand <= w[0] && hlt[h] == 1) { hlt_Mu_fired[h] = 1; hHLT_deepStudy[1]->Fill(h); break; }
+                    if(h!=0 && rand > w[h-1] && rand <= w[h] && hlt[h] == 1) { hlt_Mu_fired[h] = 1; hHLT_deepStudy[1]->Fill(h); break; }
                 }
-                else{
-                    if(hltName.Contains("HLT_Mu7_IP4")) { hlt_parking = 1; hlt_Mu_fired[0] = 1; hHLT_deepStudy[1]->Fill(0); break; }
-                    if(hltName.Contains("HLT_Mu8_IP3")) { hlt_parking = 1; hlt_Mu_fired[1] = 1; hHLT_deepStudy[1]->Fill(1); break; }
-                    if(hltName.Contains("HLT_Mu8_IP5")) { hlt_parking = 1; hlt_Mu_fired[2] = 1; hHLT_deepStudy[1]->Fill(2); break; }
-                    if(hltName.Contains("HLT_Mu9_IP5")) { hlt_parking = 1; hlt_Mu_fired[3] = 1; hHLT_deepStudy[1]->Fill(3); break; }
-                    if(hltName.Contains("HLT_Mu9_IP6")) { hlt_parking = 1; hlt_Mu_fired[4] = 1; hHLT_deepStudy[1]->Fill(4); break; }
-                    if(hltName.Contains("HLT_Mu12_IP6")) { hlt_parking = 1; hlt_Mu_fired[5] = 1; hHLT_deepStudy[1]->Fill(5); break; }
+                else{ // data
+                    if(hlt[h] == 1){ hlt_Mu_fired[h] = 1; hHLT_deepStudy[1]->Fill(h); break; }
                 }
             }
         }
